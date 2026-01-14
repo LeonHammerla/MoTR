f057fab084defb3fa90f00c73ac3fab0a42266ad
LeonHammerla <leon.hammerla@gmx.de>
2026-01-14 12:14:03 +0100

    fixing ports and allowing for all hosts (to access via domain)

--- MoTR/run_motr_in_magpie/provo/vue.config.js
+++ MoTR/run_motr_in_magpie/provo/vue.config.js
@@ -19,13 +19,19 @@ module.exports = {
     lintStyleOnBuild: false,
     stylelint: {}
   },
+  //devServer: {
+  //  disableHostCheck: true,
+  //  host: '0.0.0.0'
+  //},
     devServer: {
-    allowedHosts: 'all'
-  },
-  	publicPath:
+      allowedHosts: ['leichtesprache.neglab.de'],
+      host: '0.0.0.0'
+    },
+  publicPath:
     process.env.NODE_ENV === 'production'
       ? '/MoTR/MoTR/run_motr_in_magpie/provo/'
       : '/'
+
 //   publicPath:
 //     process.env.NODE_ENV === 'production' && process.env.REPO_NAME
 //       ? '/' + process.env.REPO_NAME + '/'
