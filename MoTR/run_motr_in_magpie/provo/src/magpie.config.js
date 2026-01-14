7a68208154f9f6bce69b2f284bbc427b9f83098e
LeonHammerla <leon.hammerla@gmx.de>
2026-01-14 17:31:59 +0100

    added directLink and correct Link to experiment website?

--- MoTR/run_motr_in_magpie/provo/src/magpie.config.js
+++ MoTR/run_motr_in_magpie/provo/src/magpie.config.js
@@ -5,9 +5,12 @@ export default {
   socketUrl: 'wss://magpie.neglab.de/socket',
   // socketUrl: 'wss://gentle-everglades-00112.herokuapp.com/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1I4D2OB',
-  contactEmail: 'cui.ding@uzh.ch',
+  //completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1I4D2OB',
+    completionUrl: 'https://experiments.neglab.de/~eliwagne/exps/Tests/main/experiment.html',
+  // contactEmail: 'cui.ding@uzh.ch',
+    contactEmail: 'eichler@lingua.uni-frankfurt.de',
   // Either 'debug', 'directLink' or 'prolific' .
-  mode: 'prolific',
+  // mode: 'prolific',
+    mode: 'directLink',
   language: 'en'
 };
