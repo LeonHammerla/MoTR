e70e1c157a952b29443311bba29439b23eff56d1
Krokodil97 <lisaeichler23@gmail.com>
2026-01-22 15:59:40 +0100

    Update magpie.config.js

--- MoTR/run_motr_in_magpie/provo/src/magpie.config.js
+++ MoTR/run_motr_in_magpie/provo/src/magpie.config.js
@@ -1,5 +1,5 @@
 export default {
-  experimentId: '2',
+  experimentId: '1',
   serverUrl: 'https://magpie.neglab.de',
   // serverUrl: 'https://gentle-everglades-00112.herokuapp.com/',
   socketUrl: 'wss://magpie.neglab.de/socket',
